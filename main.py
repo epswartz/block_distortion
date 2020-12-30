450fb6bac41ac5701663abe0acca32c2761c6e86
epswartz <eswartzen@gmail.com>
2020-12-30 00:23:31 -0500

    help text

--- main.py
+++ main.py
@@ -14,8 +14,18 @@ import typer
 app = typer.Typer()
 
 @app.command()
-def animate(input: str, frames: int = 300, duration: int = 100,  splits: int = 2000, out: str="./output.gif", orientation: str = "alternating"):
+def animate(input: str = typer.Argument(..., help="Input file (png, jpg, etc)"),
+            frames: int = typer.Option(300, help="Number of frames in output gif"),
+            duration: int = typer.Option(100, help="Duration of each frame in output gif (ms)"),
+            splits: int = typer.Option(2000, help="Number of times to split the image"),
+            out: str="./output.gif",
+            orientation: str = "alternating"
+    ):
     """
     From the given image, produce a gif with glitch effects.
     """
     # TODO call a bunch of utils stuff here
+    print("Testing")
+
+if __name__ == "__main__":
+    app()
